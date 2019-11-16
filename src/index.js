16a1d3b2a8969fd75a6289ddcd94f2a59a263bf7
VictorVitkov <VictorVitkov@genielifesciences.com>
2019-11-16 14:14:06 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message})
+    );
+    console.log('My component was rendered to the screen')
+  };
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it re-rendered!')
+  };
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          <img className="summer" class="ui medium circular image" src="https://i.pinimg.com/originals/24/8c/c4/248cc4eec11b158d6eaf49c7088022a4.jpg" />
+          <h1>Error: {this.state.errorMessage}</h1>
+        </div>
+      )
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner message={"Patience is a Virtue"} />
+  }
+
+  render() {
+    return(
+      <div className="border-black">
+        {this.renderContent()}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
